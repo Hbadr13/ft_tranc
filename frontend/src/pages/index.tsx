11309abc564b0e14a0432625f75c1fc9a6e7455d
hbadr <hamza1937badr@gmail.com>
2023-10-07 12:08:22 +0000

    to mac

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { Navbar } from '@/components/model'
+
+
+export default function Home() {
+  return (
+    <>
+      {/* <Navbar /> */}
+    </>
+
+  )
+}
---
+++ game/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  images: {
+    domains: ['tailwindcss.com', 'pbs.twimg.com'],
+  },
+}
