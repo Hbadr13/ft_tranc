e4e705980d2eeb23e6534193b423da7daab5405e
Hamza13 <hamza1937badr@gmail.com>
2023-10-11 15:49:22 +0100

    verion02

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ChatGateway } from './chat.gateway';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { FriendsModule } from './friends/friends.module';
+import { JwtModule } from '@nestjs/jwt';
 import { GameGateway } from './game/game.gateway';
 
+
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [ChatGateway, GameGateway],
+    imports: [
+        JwtModule.register({
+            secret: process.env.JWT_SECRET, // Replace with your actual secret key
+            signOptions: { expiresIn: '1h' }, // Set the token expiration time
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true
+        }), AuthModule, UserModule, BookmarkModule, PrismaModule, FriendsModule],
+    providers: [GameGateway],
 })
-export class AppModule {}
+export class AppModule { }
