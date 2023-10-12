6358e25be73ab365708a9913e98521dbf76065aa
Hamza13 <hamza1937badr@gmail.com>
2023-10-12 16:03:11 +0100

    hbadra

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -26,8 +26,8 @@ export class UserService {
   //t5arbi9a
   async findAllUsers2(userAId: number) {
     const users = await this.prisma.user.findMany();
-    const filteredUsers = users.filter(user => user.id !== userAId);
-    return filteredUsers;
+    const filteredUsers1234 = users.filter(user => user.id !== userAId);
+    return filteredUsers1234;
   }
 
 }
