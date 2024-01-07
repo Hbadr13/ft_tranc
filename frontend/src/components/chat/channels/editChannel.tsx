ba8b9fb1b72a4422b2c68bc6877f867f48425898
ebouabba <ebouabbadi@gmail.com>
2024-01-07 18:29:47 +0100

    push

--- frontend/src/components/chat/channels/editChannel.tsx
+++ frontend/src/components/chat/channels/editChannel.tsx
@@ -176,11 +176,10 @@ export default function EditChannel({ setMyStatusInRoom, currentUser, Room }: { 
                         ))}
                     </div>
                 </div>
-
             </div>
             {
 
-                click != 0 && <div className=' bg-white  dark:bg-gray-700 h-80 w-[87%]  flex-col  rounded-lg flex justify-start items-start  shadow-md sdhadow -mt-[630px] z-30'>
+                click != 0 && <div className=' bg-white  dark:bg-gray-700 h-80 w-[87%]  flex-col  rounded-lg flex justify-start items-start  shadow-md sdhadow -mt-[630px] z-30 bg-ofrange-500'>
                     {
                         click == 1 && <>
 
@@ -271,21 +270,23 @@ export default function EditChannel({ setMyStatusInRoom, currentUser, Room }: { 
                                             <button type="submit" onClick={() => upadategroup()} className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-30  px-5 py-2.5 text-center dark:bg-blue-600 justify-center items-center flex dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                                         </div>
                                         {/* </div> */}
-
-
                                     </div>
-
                                 }
                             </div>
                         </>
                     }
 
-
-
-
                     {
-                        click == 2 && <div className='w-full flex flex-col bg-fslate-400'>
-                            <h1 className='-mt-8  flex justify-center text-CusColor_primary w-[90%]'>{participant?.username}</h1>
+                        click == 2 && <div className='w-full flex flex-col bg-fslate-400 bfg-slate-700'>
+                            <div className='flex w-full h-10'>
+                                <h1 className='w-full flex justify-center items-center text-rose-600'>{participant?.username}</h1>
+                                <button className='w-full flex justify-end items-center  mr-2' onClick={() => setClick(0)} >
+                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="22" height="22" viewBox="0 0 30 30">
+                                        <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path>
+                                    </svg>
+
+                                </button>
+                            </div>
                             {userStatusRoom?.isOwner && <button className='border p-2' onClick={() => handlParticipants('admin')}>set this user is admin</button>}
                             <button className='border p-2' onClick={() => handlParticipants('kick')}>kick this user</button>
                             {userStatusRoom?.isOwner && <button className='border p-2' onClick={() => handlParticipants('inAdmin')}>inAdmin this user</button>}
--- frontend/src/pages/test/index.tsx
+++ frontend/src/pages/test/index.tsx
@@ -13,40 +13,9 @@ const Index = () => {
   };
 
   return (
-    <div className=''>
-      <form className='flex flex-col items-center justify-center m-20' onSubmit={handleButtonClick}>
-        <label>
-          Name:
-          <input required type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Description:
-          <input required type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-        </label>
-        <br />
-
-        <br />
-        <label>
-          Type:
-          <select onChange={(e) => setType(e.target.value)}>
-            <option value="public">Public</option>
-            <option value="protected">Protected</option>
-            <option value="private">Private</option>
-          </select>
-        </label>
-        {
-          Type == 'protected' &&
-          <label>
-            Password:
-            <input required type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </label>
-        }
-        <br />
-        <button>Submit</button>
-        <br />
-        {message && <p>{message}</p>}
-      </form>
+    <div className='flex justify-center items-center mt-24'>
+      
+      <div className=' w-10 h-10 bg-slate-400 flex justify-center items-center rounded-full'>A</div>
     </div >
   );
 };
